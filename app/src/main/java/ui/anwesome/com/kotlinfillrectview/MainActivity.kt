6366f4f6480259c8b6f761477e8470daadeb6f2f
Anwesh43 <anweshthecool0@gmail.com>
2018-03-01 00:18:01 +0530

    adding OnFillListener in Activity

--- app/src/main/java/ui/anwesome/com/kotlinfillrectview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinfillrectview/MainActivity.kt
@@ -3,13 +3,17 @@ package ui.anwesome.com.kotlinfillrectview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import ui.anwesome.com.fillrectview.FillRectView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        FillRectView.create(this)
+        val view = FillRectView.create(this)
+        view.addOnFillListener {
+            Toast.makeText(this, "filled ${it}", Toast.LENGTH_SHORT).show()
+        }
         fullScreen()
     }
 }
