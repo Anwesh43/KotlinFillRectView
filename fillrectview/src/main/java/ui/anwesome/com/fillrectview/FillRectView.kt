d2b4e683ec3deda114c21d6f609fa005ef166dc8
Anwesh43 <anweshthecool0@gmail.com>
2018-02-28 09:07:28 +0530

    adding FillRectView class

--- fillrectview/src/main/java/ui/anwesome/com/fillrectview/FillRectView.kt
+++ fillrectview/src/main/java/ui/anwesome/com/fillrectview/FillRectView.kt
@@ -3,3 +3,20 @@ package ui.anwesome.com.fillrectview
 /**
  * Created by anweshmishra on 28/02/18.
  */
+import android.content.*
+import android.graphics.*
+import android.view.*
+class FillRectView(ctx : Context) : View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas : Canvas) {
+
+    }
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
